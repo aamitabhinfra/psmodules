fdc10ab0d4c7e262dbe06e6fc61a6b4edf36e116
amitabh <buddhabacchan@gmail.com>
2020-04-24 14:11:33 -0700

    test download script using play.txt

---
+++ scripts/windows10-boxstarter/play.bat
@@ -0,0 +1,1 @@
+START http://boxstarter.org/package/url?
--- scripts/windows10-install/windows10-init.ps1
+++
@@ -1,8 +0,0 @@
-#Requires -RunAsAdministrator
-
-# Make sure you run this script with elevated permissions
-Set-ExecutionPolicy Unrestricted -Scope Process -Force
-
-# Intall Boxstarter/Chocolatey
-. { Invoke-WebRequest -useb https://boxstarter.org/bootstrapper.ps1 } | Invoke-Expression; Get-Boxstarter -Force
-
