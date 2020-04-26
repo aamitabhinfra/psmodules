295770c8ec688d98995950cc83cc09d74d137b1f
amitabh <buddhabacchan@gmail.com>
2020-04-26 00:31:05 -0700

    updated install scripts

--- AmitabhCoHyperV/Tests/CreateVM-utest1804-Ubuntu1804LTS.ps1
+++ AmitabhCoHyperV/Tests/CreateVM-utest1804-Ubuntu1804LTS.ps1
@@ -1,3 +1,9 @@
 Import-Module AmitabhCoHyperV -Force
 
 New-AcoVM -name utest1804 -cpu 1 -ram 4GB -iso "Y:\Software\OS\Ubuntu\ubuntu-18.04.4-desktop-amd64.iso"
+
+# To change resolution
+#   sudo vi /etc/default/grub
+#   replace string video=hyperv_fb: 1920×1280
+#   sudo update-grub
+#   reboot
