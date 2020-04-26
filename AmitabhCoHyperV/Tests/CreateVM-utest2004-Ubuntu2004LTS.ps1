dcd2951ee20db44fe139509012d817468d8cac8b
amitabh <buddhabacchan@gmail.com>
2020-04-25 23:58:39 -0700

    updated install scripts

--- AmitabhCoHyperV/Tests/CreateVM-utest2004-Ubuntu2004LTS.ps1
+++ AmitabhCoHyperV/Tests/CreateVM-utest2004-Ubuntu2004LTS.ps1
@@ -1,3 +1,3 @@
 Import-Module AmitabhCoHyperV -Force
 
-New-AcoVM -name utest2004 -cpu 1 -ram 4GB -iso "Y:\Software\OS\Ubuntu\ubuntu-20.04.4-desktop-amd64.iso"
+New-AcoVM -name utest2004 -cpu 1 -ram 4GB -iso "Y:\Software\OS\Ubuntu\ubuntu-20.04-desktop-amd64.iso"
