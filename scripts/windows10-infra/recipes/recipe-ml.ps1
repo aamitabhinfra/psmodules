4bfe46697534458ff67c076a68231d58d88feaa2
amitabh <buddhabacchan@gmail.com>
2021-09-14 11:17:43 -0700

    moved install-base.txt to ps1

--- scripts/windows10-infra/recipes/recipe-ml.ps1
+++ scripts/windows10-infra/recipes/recipe-ml.ps1
@@ -13,7 +13,7 @@ choco install -y cascadiafonts
 Write-Host "----------------------------" -ForegroundColor Yellow
 Write-Host "Install base applicaitons" -ForegroundColor Yellow
 Write-Host "----------------------------" -ForegroundColor Yellow
-Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/aamitabhinfra/psmodules/master/scripts/windows10-infra/tasks/Install-Base.txt
+Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/aamitabhinfra/psmodules/master/scripts/windows10-infra/tasks/Install-Base.ps1
 
 Write-Host "-----------------------------" -ForegroundColor Yellow
 Write-Host "Install Applicaitons" -ForegroundColor Yellow
