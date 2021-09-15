2242c643192d04fb1ff27a6921c45be62ceeaa46
amitabh <buddhabacchan@gmail.com>
2021-09-14 23:57:11 -0700

    cleaned up recipes

--- scripts/windows10-infra/recipes/recipe-amityogatemp.ps1
+++ scripts/windows10-infra/recipes/recipe-amityogatemp.ps1
@@ -1,24 +1,3 @@
-Write-Host "-----------------------------------------" -ForegroundColor Yellow
-Write-Host "Install bootstrap: Boxstarter/Choco, etc." -ForegroundColor Yellow
-Write-Host "-----------------------------------------" -ForegroundColor Yellow
-Set-ExecutionPolicy Unrestricted -Scope Process -Force
-# Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/aamitabhinfra/psmodules/master/scripts/windows10-infra/tasks/bootstrap.ps1'))
-
-Write-Host "-----------------------------" -ForegroundColor Yellow
-Write-Host "Install Fonts" -ForegroundColor Yellow
-Write-Host "-----------------------------" -ForegroundColor Yellow
-# Reference: https://github.com/microsoft/cascadia-code/wiki/Installing-Cascadia-Code
-choco install -y cascadiafonts
-
-Write-Host "----------------------------" -ForegroundColor Yellow
-Write-Host "Install base applicaitons" -ForegroundColor Yellow
-Write-Host "----------------------------" -ForegroundColor Yellow
-# Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/aamitabhinfra/psmodules/master/scripts/windows10-infra/tasks/base.ps1
-
-Write-Host "-----------------------------" -ForegroundColor Yellow
-Write-Host "Install Applicaitons" -ForegroundColor Yellow
-Write-Host "-----------------------------" -ForegroundColor Yellow
-
 Write-Host "------------------------------------------------------------" -ForegroundColor Yellow
 Write-BoxstarterMessage "Inatalling WSL & Hyper-V, vagrant"
 Write-Host "------------------------------------------------------------" -ForegroundColor Yellow
